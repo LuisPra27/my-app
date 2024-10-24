d89339022dbc97a1f71d326548a78c09b3c08686
LuisPra27 <e0957226681@live.uleam.edu.ec>
2024-10-23 22:17:31 -0500

    RepositoryList
    Se agrego una lista de Repositorios para ejemplo y se las llamo al main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,10 @@
-import {Text, View } from 'react-native'
+import {View } from 'react-native'
 import Constants from 'expo-constants'
-
+import RepositoryList from './RepositoryList'
 const Main = () => {
     return (
         <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1}}>
-            <Text>Rate Repository Applicaction</Text>
+            <RepositoryList />
         </View>
     )
 }
--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,5 +1,58 @@
-export default[
+export default [
     {
-            
+      id: 'facebook.react',
+      fullName: 'facebook/react',
+      description: 'A declarative, efficient, and flexible JavaScript library for building user interfaces.',
+      language: 'JavaScript',
+      forksCount: 38623,
+      stargazersCount: 202045,
+      ratingAverage: 98,
+      reviewCount: 500,
+      ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/69631?v=4'
+    },
+    {
+      id: 'angular.angular',
+      fullName: 'angular/angular',
+      description: 'One framework. Mobile & desktop.',
+      language: 'TypeScript',
+      forksCount: 22749,
+      stargazersCount: 88726,
+      ratingAverage: 90,
+      reviewCount: 300,
+      ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/139426?v=4'
+    },
+    {
+      id: 'vuejs.vue',
+      fullName: 'vuejs/vue',
+      description: 'A progressive, incrementally-adoptable JavaScript framework for building UI on the web.',
+      language: 'JavaScript',
+      forksCount: 34259,
+      stargazersCount: 211456,
+      ratingAverage: 97,
+      reviewCount: 420,
+      ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/6128107?v=4'
+    },
+    {
+      id: 'nodejs.node',
+      fullName: 'nodejs/node',
+      description: 'Node.js JavaScript runtime.',
+      language: 'JavaScript',
+      forksCount: 35345,
+      stargazersCount: 98567,
+      ratingAverage: 92,
+      reviewCount: 250,
+      ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/9950313?v=4'
+    },
+    {
+      id: 'django.django',
+      fullName: 'django/django',
+      description: 'The Web framework for perfectionists with deadlines.',
+      language: 'Python',
+      forksCount: 24683,
+      stargazersCount: 68894,
+      ratingAverage: 95,
+      reviewCount: 275,
+      ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/27804?v=4'
     }
-]
+  ];
+  
