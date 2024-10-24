b509d23914a3ed8676924c911b46847c057565af
LuisPra27 <e0957226681@live.uleam.edu.ec>
2024-10-24 00:05:56 -0500

    Ordenar

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,26 +1,16 @@
 import React from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, Text } from "react-native";
 import repositories from "../data/repositories";
-
+import RepositoryItem from "./RepositoryItem.jsx"
 const RepositoryList = () => {
     return(
         <FlatList 
         data={repositories}
         ItemSeparatorComponent={() => <Text> </Text>}
         renderItem={({ item: repo }) => (
-             <View key={repo.id}>
-             <Text> id:{repo.id}</Text>
-             <Text> FullName:{repo.fullName}</Text>
-             <Text> Description: {repo.description}</Text>
-             <Text> Languaje: {repo.language}</Text>
-             <Text> Stars: {repo.stargazersCount}</Text>
-             <Text> Forks: {repo.forksCount}</Text>
-             <Text> Review: {repo.reviewCount}</Text>
-             <Text> Rating: {repo.ratingAverage}</Text>
-            </View>
+            <RepositoryItem {... repo} />
         )}
         />
     )
 }
-
 export default RepositoryList
